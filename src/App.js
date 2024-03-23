7278a353bd8489a86adb03f9302e418187ba38de
arjunramesan01 <arjun.ramesan@byjus.com>
2024-03-23 19:22:09 +0530

    code cleanup

--- src/App.js
+++ src/App.js
@@ -8,48 +8,49 @@ function App() {
   const [formData, setFormData] = useState({
     title: undefined,
     author: undefined,
-    publication_date: undefined,
+    publishedFrom: undefined,
+    publishedTo: undefined,
     genre: undefined,
     isbn: undefined,
     inStock: true,
   });
 
   const handleChange = (e) => {
+    // Updating the formData on type
     const { name, value, type, checked } = e.target;
-    setFormData(prevFormData => ({
+    setFormData((prevFormData) => ({
       ...prevFormData,
-      [name]: type === 'checkbox' ? checked : value
+      [name]: type === "checkbox" ? checked : value,
     }));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(formData);
     fetchBooks(formData);
   };
 
-
   const fetchBooks = async () => {
+    // First we fetch auth token
     const token = await getToken();
+
+    // Using this token we fetch books
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
     myHeaders.append("Authorization", "Bearer " + token);
-
     const raw = JSON.stringify(formData);
-
     const requestOptions = {
       method: "POST",
       headers: myHeaders,
       body: raw,
       redirect: "follow",
     };
-
     var books_resp = await fetch("http://localhost:8000/books", requestOptions);
     books_resp = await books_resp.json();
     setBooks(books_resp.data);
   };
 
   const getToken = async () => {
+    // Reset books 
     setBooks([]);
 
     const myHeaders = new Headers();
@@ -77,74 +78,83 @@ function App() {
   return (
     <div className="App">
       <div className="max-w-md mx-auto flex p-6 bg-white rounded-lg shadow-xl">
-      <form onSubmit={handleSubmit} className="space-y-4 max-w-md mx-auto flex flex-col w-full">
-        <input 
-          type="text" 
-          name="title" 
-          value={formData.title}
-          onChange={handleChange}
-          className="input-field" 
-          placeholder="Title" 
-        />
-        <input 
-          type="text" 
-          name="author" 
-          value={formData.author}
-          onChange={handleChange}
-          className="input-field" 
-          placeholder="Author" 
-        />
-        <input 
-          type="date" 
-          name="publication_date" 
-          value={formData.publication_date}
-          onChange={handleChange}
-          className="input-field" 
-        />
-        <input 
-          type="text" 
-          name="genre" 
-          value={formData.genre}
-          onChange={handleChange}
-          className="input-field" 
-          placeholder="Genre" 
-        />
-        <input 
-          type="text" 
-          name="isbn" 
-          value={formData.isbn}
-          onChange={handleChange}
-          className="input-field" 
-          placeholder="ISBN" 
-        />
-        <div className="flex items-center">
-          <input 
-            type="checkbox" 
-            name="inStock" 
-            checked={formData.inStock}
-            onChange={handleChange}
-            className="checkbox" 
-          />
-          <span className="ml-2">In Stock</span>
-        </div>
-        <button type="submit"  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Submit</button>
-      </form>
-{/* 
-        <div className="flex flex-col w-full">
+        <form
+          onSubmit={handleSubmit}
+          className="space-y-4 max-w-md mx-auto flex flex-col w-full text-left"
+        >
+          <label>Title</label>
           <input
             type="text"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className="mb-4 p-2 border rounded"
-            placeholder="Search for books..."
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            className="input-field"
+            placeholder="Title"
           />
+          <hr></hr>
+          <label>Author</label>
+          <input
+            type="text"
+            name="author"
+            value={formData.author}
+            onChange={handleChange}
+            className="input-field"
+            placeholder="Author"
+          />
+          <hr></hr>
+          <div className="flex gap-4">
+            <div>
+              <label>Published From</label>
+              <input
+                type="date"
+                name="publishedFrom"
+                value={formData.publishedFrom}
+                onChange={handleChange}
+                className="input-field flex-1"
+                placeholder="Published From"
+              />
+            </div>
+
+            <div>
+              <label>Published To</label>
+              <input
+                type="date"
+                name="publishedTo"
+                value={formData.publishedTo}
+                onChange={handleChange}
+                className="input-field flex-1"
+                placeholder="Published To"
+              />
+            </div>
+          </div>
+          <hr></hr>
+          <label>Genre</label>
+          <input
+            type="text"
+            name="genre"
+            value={formData.genre}
+            onChange={handleChange}
+            className="input-field"
+            placeholder="Genre"
+          />
+          <hr></hr>
+          <div className="flex items-center">
+            <input
+              type="checkbox"
+              name="inStock"
+              checked={formData.inStock}
+              onChange={handleChange}
+              className="checkbox"
+            />
+            <span className="ml-2">Show In Stock Only</span>
+          </div>
           <button
-            onClick={fetchBooks}
+            type="submit"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
-            Search
+            Submit
           </button>
-        </div> */}
+        </form>
       </div>
       <div className="mt-4">
         <div className="App">
@@ -160,9 +170,6 @@ function App() {
                 <p className="text-gray-600 text-xs">Genre: {book.genre}</p>
                 <p className="mt-2 text-gray-700 text-sm">{book.description}</p>
                 <p className="text-gray-600 text-xs">ISBN: {book.isbn}</p>
-                <p className="text-gray-600 text-xs">
-                  Stock Count: {book.stock_count}
-                </p>
               </div>
             ))}
           </div>
